0205ec85d28f15df31074ff88eff6848f806128b
chaser <1070045244@qq.com>
2019-07-20 23:42:08 +0800

    modify: cdn && site url

--- sw.js
+++ sw.js
@@ -10,9 +10,7 @@ const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "cdn.bootcss.com"
 ]
 
 
