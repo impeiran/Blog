b96c63ed63af025399c850e1fed9e728bcaba854
impeiran <1070045244@qq.com>
2020-04-23 21:24:41 +0800

    chore: remove promise test polyfill

--- promise/promise.js
+++ promise/promise.js
@@ -166,14 +166,4 @@ Promise.all = promises => {
   })
 }
 
-Promise.defer = Promise.deferred = function () {
-  const dfd = {}
-  dfd.promise = new Promise((resolve, reject) => {
-    dfd.resolve = resolve
-    dfd.reject = reject
-  })
-  return dfd
-}
-
-// export default Promise
 module.exports = Promise
